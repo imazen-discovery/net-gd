a53959e74ad05b3adf600e7426716a8028619f07
Chris Reuter <chris@blit.ca>
2014-01-07 16:39:41 -0500

    Added a testcase running some of the functions.

--- unit-tests/image_test.cs
+++ unit-tests/image_test.cs
@@ -15,16 +15,24 @@ namespace GD {
       Assert.Greater(mj, 0);
       Assert.AreEqual(Image.versionString, mj + "." + mn + "." + rv + ex);
     }
+
+    [Test]
+    public void BasicCall() {
+      Image im = new Image(10, 10);
+    
+      int red = im.colorClosest(255, 0, 0);
+      Assert.Greater(red, 0);
+
+      im.filledRectangle(0, 0, 9, 9, red);
+      Assert.AreEqual(im.getPixel(5, 5), red);
+
+      Assert.IsTrue(im.grayScale());
+
+      im.Dispose();
+    }
+
+
+
   }
 
-
-  // [Test]
-  // public void BasicCall() {
-  //   Image im = new Image(10, 10);
-
-  //   im.colorClosest(, 0, 0);
-
-
-
-  // }
 }
