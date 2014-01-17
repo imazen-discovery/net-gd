8857c01cda40039c69975ba61a35cb05016e52f1
Chris Reuter <chris@blit.ca>
2014-01-17 15:40:18 -0500

    Added ImageData class skeleton.

---
+++ lib/image_data.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+
+namespace GD {
+  using Internal;
+
+  class ImageData {
+    
+    public ImageData(string filename) {
+    }
+
+    public ImageData(Image img, Enc type = Enc.GD) {
+    }
+
+    public void save(Stream outstream) {
+    }
+
+    public Image decode() {
+
+    }
+
+  }
+
+}
