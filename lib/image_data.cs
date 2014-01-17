8c897d2a9f1bb10e5fcc776421dbe4515296bece
Chris Reuter <chris@blit.ca>
2014-01-17 16:20:12 -0500

    Checkpoint: image_data.cs compiles.

--- lib/image_data.cs
+++ lib/image_data.cs
@@ -4,7 +4,27 @@ using System.IO;
 namespace GD {
   using Internal;
 
+  public enum Enc {
+    GIF, GD, GD2, WBMP, BMP, TGA, PNG, JPG, TIFF, WEBP, XPM, 
+  }
+
+  internal class FileType {
+    public delegate void ReaderFn(int size, byte[] data);
+
+    public readonly string extension;
+    public readonly Enc type;
+    public readonly ReaderFn reader;
+    
+    public FileType(string ext, Enc t, ReaderFn fn) {
+      extension = ext;
+      type = t;
+      reader = fn;
+    }
+  }
+
+
   class ImageData {
+    byte[] data;
     
     public ImageData(string filename) {
     }
@@ -16,8 +36,11 @@ namespace GD {
     }
 
     public Image decode() {
-
+      return null;
     }
+
+    
+    static private FileType[] types = null;
 
   }
 
