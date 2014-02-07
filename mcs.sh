dabb4e4d7f0cfb0a3e49976fbd3e6b7d8ee06b1a
Chris Reuter <chris@blit.removethispart.ca>
2014-02-06 20:18:41 -0500

    Checkpoint; nothing working yet.
    Adapting existing make-based build process to MS.NET and MSys.

--- mcs.sh
+++ mcs.sh
@@ -2,5 +2,10 @@
 
 # Wrapper around mcs to make it easier to switch versions.
 
-dmcs -sdk:4 $*
-exit $?
+if [`uname -o` = 'Msys' ]; then
+    export PATH="/c/progra~1/MSBuild/12.0/Bin:$PATH"
+    csc $*
+else
+    dmcs -sdk:4 $*
+    exit $?
+fi
