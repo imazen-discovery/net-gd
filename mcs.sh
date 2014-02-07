3ea55ef0355e3adeac497cab188892a357d4f937
Chris Reuter <chris@blit.ca>
2014-02-07 14:49:03 -0500

    Fixed quoting in mcs.sh.

--- mcs.sh
+++ mcs.sh
@@ -2,7 +2,7 @@
 
 # Wrapper around mcs to make it easier to switch versions.
 
-if [`uname -o` = 'Msys' ]; then
+if [ "`uname -o`" = 'Msys' ]; then
     export PATH="/c/progra~1/MSBuild/12.0/Bin:$PATH"
     csc $*
 else
