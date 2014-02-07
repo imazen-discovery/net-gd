958fd9298293fb161965f9ead5d7012c1347a0c0
Chris Reuter <chris@blit.ca>
2014-02-07 18:25:46 -0500

    Consolidated config scripts into cfg.sh; works on Linux.

---
+++ cfg.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Script to encapsulate all of the config information.  Think of this
+# as a bad, hacky reinvention of pkg-config.
+
+
+#
+# Edit these as necessary:
+#
+
+# ID of the platform we're running on.  Override if your uname does
+# the wrong thing.
+PLATFORM="`uname -o`"
+
+# Path to your .net compiler on Windows
+DOTNET_PATH="/c/progra~1/MSBuild/12.0"
+
+# Name+args of your preferred Mono compiler
+MONO_MCS='dmcs -sdk:4'
+
+# Path (relative to the location of this script) of the directory
+# containing the GD source tree we're using.
+GD_DIR=../gd-libgd/
+
+
+# ---------------------------------------------------------------------------
+
+# Set per-platform values
+if [ "$PLATFORM" = 'Msys' ]; then
+    PATHVAL="$DOTNET_PATH/Bin:$PATH"
+    MCS=csc
+elif [ "$PLATFORM" = "GNU/Linux" ]; then
+    PATHVAL="$PATH"
+    MCS="$MONO_MCS"
+else
+    echo "Unknown platform: '$PLATFORM'.  Edit cfg.sh to match your platform."
+    exit 1
+fi
+
+# Figure out what the caller wants and output it
+case $* in
+    path)
+        echo $PATHVAL
+        ;;
+    compiler)
+        echo $MCS
+        ;;
+    gdpath)
+        (cd `dirname $0`; readlink -m "$GD_DIR")
+        ;;
+    *)
+        echo "Invalid argument."
+        exit 1
+        ;;
+esac
+
+exit 0
